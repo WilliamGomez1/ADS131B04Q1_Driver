3be0dfbca684e33414bd480a6fa98386cd9d7d1d
WilliamGomez1 <180569402+WilliamGomez1@users.noreply.github.com>
2025-01-27 20:13:56 -0500

    changes to functions

--- ADS131B04Q1.c
+++ ADS131B04Q1.c
@@ -15,19 +15,49 @@ int ADS131B04Q1_Init(SPI_HandleTypeDef* hspi, GPIO_TypeDef* GPIOpinLetter, uint1
 	ADS131_hspi = hspi;
 	ADS131pinLetter = GPIOpinLetter;
 	ADS131_PIN_Number = GPIO_PIN_Number;
+	int status = 0;
 
+	//begin by ensuring CS line is high impedance
+	HAL_GPIO_WritePin(ADS131pinLetter, ADS131_PIN_Number, GPIO_PIN_SET);
+
+	//check if ADC is connected
+	status = ADS131B04Q1_CheckConnection();
+	if(status == HAL_ERROR){
+		return status; //if ADC is not connected, return 1
+	}
+
+	//drive CS line low
 	HAL_GPIO_WritePin(ADS131pinLetter, ADS131_PIN_Number, GPIO_PIN_RESET);
 
 
 	return 0;
 }
 
-int ADS131B04Q1_CheckConnection(GPIO_TypeDef* GPIOpinLetter, uint16_t GPIO_PIN_Number){
+int ADS131B04Q1_CheckConnection(){
+	int status = 0;
+	uint8_t RxData[2] = {0,0};
 
+	//Read nnn nnnn plus 1 registers beginning at address a aaaa a: 101a aaaa annn nnnn
+	//reading register 00h, only reading first register so nnn nnnn should be 000 0000
+	//command should be 0x1010, 0x0000, 0x0000, 0x0000
+	uint8_t TxCommand[8] = {0x10, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+	//drive CS line low
+	HAL_GPIO_WritePin(ADS131pinLetter, ADS131_PIN_Number, GPIO_PIN_RESET);
+
+	//send no command, which the ADS131B04 will read as "NULL" to which it will respond with a status byte
+	status = HAL_SPI_Transmit(ADS131_hspi, TxCommand, 8, 1000);
+	if(status == HAL_ERROR){
+			return status;
+	}
+	status = HAL_SPI_Receive (ADS131_hspi, RxData, 2, 1000);
+
+	return 0;
 }
 
 
-int ADS131B04Q1_ReadChannel(GPIO_TypeDef* GPIOpinLetter, uint16_t GPIO_PIN_Number, int32_t *channelReading){
+int ADS131B04Q1_ReadChannels(int32_t *channelReading1, int32_t *channelReading2, int32_t *channelReading3, int32_t *channelReading4){
 
+
+	return 0;
 }
 
--- ADS131B04Q1.h
+++ ADS131B04Q1.h
@@ -21,9 +21,9 @@ extern uint16_t ADS131_PIN_Number;
 int ADS131B04Q1_Init(SPI_HandleTypeDef* hspi, GPIO_TypeDef* GPIOpinLetter, uint16_t GPIO_PIN_Number);
 
 // Checks if the ADS131B04-Q1 is connected to the SPI bus, returns 0 if successful, 1 if failed
-int ADS131B04Q1_CheckConnection(GPIO_TypeDef* GPIOpinLetter, uint16_t GPIO_PIN_Number);
+int ADS131B04Q1_CheckConnection();
 
 // Reads voltage calculated from ADS131B04-Q1, returns 0 if successful, 1 if failed
-int ADS131B04Q1_ReadChannel(GPIO_TypeDef* GPIOpinLetter, uint16_t GPIO_PIN_Number, int32_t *channelReading);
+int ADS131B04Q1_ReadChannels(int32_t *channelReading1, int32_t *channelReading2, int32_t *channelReading3, int32_t *channelReading4);
 
 #endif /* ADS131B04Q1_DRIVER_ADS131B04Q1_H_ */
