9fd736d621f255ce6e03a65bf6593e29623cdb37
WilliamGomez1 <180569402+WilliamGomez1@users.noreply.github.com>
2025-01-27 20:23:21 -0500

    Change CheckConnection to assume command is sent as 2 bytes

--- ADS131B04Q1.c
+++ ADS131B04Q1.c
@@ -39,17 +39,17 @@ int ADS131B04Q1_CheckConnection(){
 
 	//Read nnn nnnn plus 1 registers beginning at address a aaaa a: 101a aaaa annn nnnn
 	//reading register 00h, only reading first register so nnn nnnn should be 000 0000
-	//command should be 0x1010, 0x0000, 0x0000, 0x0000
-	uint8_t TxCommand[8] = {0x10, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+	//command should be 0b10100000, 0b00000000
+	uint8_t TxCommand[2] = {0b10100000, 0b00000000};
 	//drive CS line low
 	HAL_GPIO_WritePin(ADS131pinLetter, ADS131_PIN_Number, GPIO_PIN_RESET);
 
 	//send no command, which the ADS131B04 will read as "NULL" to which it will respond with a status byte
-	status = HAL_SPI_Transmit(ADS131_hspi, TxCommand, 8, 1000);
+	status = HAL_SPI_Transmit(ADS131_hspi, TxCommand, 2, HAL_MAX_DELAY);
 	if(status == HAL_ERROR){
 			return status;
 	}
-	status = HAL_SPI_Receive (ADS131_hspi, RxData, 2, 1000);
+	status = HAL_SPI_Receive (ADS131_hspi, RxData, 2, HAL_MAX_DELAY);
 	//todo: make sure MSB goes into RxData[0]
 	if(RxData[0] == 0b01000100){
 		return 0;
